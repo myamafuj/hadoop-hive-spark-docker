0b84ab9d1fc65e4f7ee8c5abb28c87b700d2699a
Masaya TAKAFUJI <takafuji.masaya@gmail.com>
2022-02-14 13:58:37 +0900

    [Hadoop] Enable hive server

--- master/run.sh
+++ master/run.sh
@@ -15,4 +15,18 @@ echo "Starting Hadoop resource manager..."
 yarn --daemon start resourcemanager
 
 echo "Starting Hadoop history server..."
-mapred historyserver
+mapred --daemon start historyserver
+
+if [ ! -f "$NAMEDIR/initialized" ]; then
+  echo "Configuring Hive..."
+  hadoop fs -mkdir -p    /user/hive/warehouse
+  hadoop fs -chmod g+w   /user/hive/warehouse
+  schematool -dbType postgres -initSchema
+  touch "$NAMEDIR/initialized"
+fi
+
+echo "Starting Hive Metastore..."
+hive --service metastore &
+
+echo "Starting Hive server2..."
+hiveserver2
